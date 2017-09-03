515f47415d3828269c9d67fa711897a8d40c6a8c
niel <niel.heo@gmail.com>
2017-09-03 14:31:56 +0700

    New Project TravelConnect.Api

---
+++ TravelConnect.Api/Controllers/AirportAutocompleteController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TravelConnect.Models.Responses;
+using TravelConnect.Interfaces;
+
+namespace TravelConnect.Api.Controllers
+{
+    [Route("api/airportautocomplete")]
+    public class AirportAutocompleteController : Controller
+    {
+        private IGeoService _GeoService;
+
+        public AirportAutocompleteController(IGeoService _GeoService)
+        {
+            this._GeoService = _GeoService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<AirportAutocompleteRS> Get(string query)
+        {
+            AirportAutocompleteRS response = await _GeoService.GetAirportAutocompleteAsync(query);
+
+            return response;
+        }
+    }
+}
---
+++ TravelConnect.Api/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using TravelConnect.Models;
+using Microsoft.EntityFrameworkCore;
+using TravelConnect.Sabre;
+using TravelConnect.Interfaces;
+using TravelConnect.Services;
+
+namespace TravelConnect.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            var connection =
+                    "Data Source=(local);" +
+                    "Initial Catalog=TravelConnect;" +
+                    "User id=sa;" +
+                    "Password=123qwe!@#Q;";
+
+            services.AddDbContext<TCContext>(options => options.UseSqlServer(connection, b => b.MigrationsAssembly("TravelConnect.Web")));
+
+            services.AddTransient<ISabreConnector, SabreConnector>();
+            services.AddTransient<IGeoService, GeoService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
