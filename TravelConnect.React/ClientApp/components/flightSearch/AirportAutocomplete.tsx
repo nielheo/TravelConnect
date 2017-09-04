63ed90e998fbf04ba9782f156ffaafad8f56a5df
niel <niel.heo@gmail.com>
2017-09-04 21:10:08 +0700

    refactor airportAutocemplete

---
+++ TravelConnect.React/ClientApp/components/flightSearch/AirportAutocomplete.tsx
@@ -0,0 +1,48 @@
+﻿import * as React from 'react';
+
+const typeahead = require('react-bootstrap-typeahead') as any;
+const AsyncTypeahead = typeahead.asyncContainer(typeahead.Typeahead) as any;
+
+export default class AirportAutocomplete extends React.Component<{onChange: any, label: any}, any> {
+    constructor() {
+        super();
+        this.state = {
+            isReturn: true,
+            origin: {},
+            destination: {}
+        };
+    }
+
+    toggleIsReturn = () => {
+        this.setState({
+            isReturn: !this.state.isReturn
+        })
+    }
+
+
+    public render() {
+        console.log(this.state)
+        return <div>
+            <label>{this.props.label}</label>
+            <AsyncTypeahead
+                onChange={this.props.onChange}
+                onSearch={(query: any) => (
+                    fetch(`/api/airportautocomplete?query=${query}`)
+                        .then(resp => resp.json())
+                        .then(json => {
+                            this.setState({
+                                options: json.airportsRS.map((item: any) => {
+                                    return {
+                                        id: item.code,
+                                        label: item.fullName
+                                    }
+                                })
+                            })
+                        })
+                )}
+                delay={500}
+                options={this.state.options}
+            />
+        </div>
+    }
+}
--- TravelConnect.React/ClientApp/components/flightSearch/Index.tsx
+++ TravelConnect.React/ClientApp/components/flightSearch/Index.tsx
@@ -1,10 +1,7 @@
 ﻿import * as React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 
-const typeahead = require('react-bootstrap-typeahead') as any;
-
-//const Autocomplete = require("react-autocomplete") as any;
-const AsyncTypeahead = typeahead.asyncContainer(typeahead.Typeahead) as any;
+import AirportAutocomplete from './AirportAutocomplete'
 
 export default class FlightSearch extends React.Component<RouteComponentProps<{}>, any> {
     constructor() {
@@ -23,19 +20,25 @@ export default class FlightSearch extends React.Component<RouteComponentProps<{}
     }
 
     _handleOriginChanged = (selectedItem: any) => {
+        console.log(selectedItem)
         this.setState({
-            origin: selectedItem
+            origin: selectedItem[0]
         })
     }
 
     _handleDestinationChanged = (selectedItem: any) => {
         this.setState({
-            destination: selectedItem
+            destination: selectedItem[0]
         })
     }
 
+    _handleOnClick = () => {
+        console.log("----")
+        console.log(this.state)
+    }
+
     public render() {
-        console.log(this.state.isReturn)
+        console.log(this.state)
         return <div>
             <h1>Search your flight</h1>
             <table className="form-horizontal">
@@ -49,47 +52,16 @@ export default class FlightSearch extends React.Component<RouteComponentProps<{}
                     </tr>
                     <tr>
                         <td cellPadding='5'>
-                            <label>Origin Airport</label>
-                            <AsyncTypeahead
+                            <AirportAutocomplete
                                 onChange={this._handleOriginChanged}
-                                onSearch={(query: any) => (
-                                    fetch(`/api/airportautocomplete?query=${query}`)
-                                        .then(resp => resp.json())
-                                        .then(json => {
-                                            this.setState({
-                                                options: json.airportsRS.map((item: any) => {
-                                                    return {
-                                                        id: item.code,
-                                                        label: item.fullName
-                                                    }
-                                                })
-                                            })
-                                        })
-                                )}
-                                delay={500}
-                                options={this.state.options}
+                                label="Origin Airport"
+                                
                             />
                         </td>
                         <td cellPadding='5'>
-                            <label>Destination Airport</label>
-                            <AsyncTypeahead
+                            <AirportAutocomplete
                                 onChange={this._handleDestinationChanged}
-                                onSearch={(query: any) => (
-                                    fetch(`/api/airportautocomplete?query=${query}`)
-                                        .then(resp => resp.json())
-                                        .then(json => {
-                                            this.setState({
-                                                options: json.airportsRS.map((item: any) => {
-                                                    return {
-                                                        id: item.code,
-                                                        label: item.fullName
-                                                    }
-                                                })
-                                            })
-                                        })
-                                )}
-                                delay={500}
-                                options={this.state.options}
+                                label="Destination Airport"
                             />
                         </td>
                     </tr>
@@ -101,6 +73,11 @@ export default class FlightSearch extends React.Component<RouteComponentProps<{}
                         <td cellPadding='5'>
                             <label>Return Date</label>
                             <input type="text" className='form-control' disabled={!this.state.isReturn} />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td cellPadding={5}>
+                            <button onClick={this._handleOnClick}>Search</button>
                         </td>
                     </tr>
                 </tbody>
