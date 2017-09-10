436ef6da1f1b141c8e05d3aa498b1c02bb7970b7
niel <niel.heo@gmail.com>
2017-09-10 12:22:53 +0700

    Display routes in FlightDetail

--- TravelConnect.React/ClientApp/components/flightResult/FlightDetail.tsx
+++ TravelConnect.React/ClientApp/components/flightResult/FlightDetail.tsx
@@ -2,11 +2,15 @@
 
 import * as moment from 'moment'
 
+import * as Commons from '../Commons'
+
 export default class FlightDetails extends React.Component<{ segment: any }, any> {
   constructor(props: any) {
     super(props)
     this.state = {
       showDetails: false,
+      originAirport: null,
+      destinationAirport: null,
     }
   }
 
@@ -23,10 +27,19 @@ export default class FlightDetails extends React.Component<{ segment: any }, any
     }
   }
 
+  componentDidMount() {
+    console.log('FlightDetail-DidMount')
+    const { segment } = this.props
+    Commons._GetAirport(segment.origin).then(res => { this.setState({ originAirport: res }) })
+    Commons._GetAirport(segment.destination).then(res => { this.setState({ destinationAirport: res }) })
+  }
+
 
   public render() {
     const { segment } = this.props
-    return <div className='row col-md-12'>
+    const { originAirport, destinationAirport } = this.state
+    return <section>
+    <div className='row col-md-12'>
       <div className='col-md-3'>
         <h4> {moment(segment.departure.time).format('HH:mm')}</h4>
       </div>
@@ -37,5 +50,42 @@ export default class FlightDetails extends React.Component<{ segment: any }, any
         <h4> {this._durationFormat(segment.elapsed)}</h4>
       </div>
     </div>
+    {
+      originAirport && destinationAirport &&
+      <section>
+        <div className='row col-md-12'>
+          <div className='col-md-3'>
+
+          </div>
+          <div className='col-md-9'>
+              {originAirport.cityName} to {destinationAirport.cityName}
+          </div>
+        
+        </div>
+        <div className='row col-md-12'>
+          <div className='col-md-3'>
+
+          </div>
+          <div className='col-md-9'>
+            {originAirport.name + ' (' + originAirport.code + ')'} to {destinationAirport.name + ' (' + destinationAirport.code + ')'}
+          </div>
+
+        </div>
+        <div className='row col-md-12'>
+          <div className='col-md-3'>
+
+          </div>
+          <div className='col-md-9'>
+              {segment.marketingFlight.airline + ' ' + segment.marketingFlight.number}
+
+              {segment.marketingFlight.airline !== segment.operatingFlight.airline
+                ? ' flight by ' + segment.operatingFlight.airline + ' ' + segment.operatingFlight.number
+                : ''}
+          </div>
+
+        </div>
+      </section>
+    }
+    </section>
   }
 }
