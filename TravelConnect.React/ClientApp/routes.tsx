563bf3f954ac74b6058202ca7797157baba51570
niel <niel.heo@gmail.com>
2017-12-25 18:56:29 +0700

    Add Reduc Store for Hotel

--- TravelConnect.React/ClientApp/routes.tsx
+++ TravelConnect.React/ClientApp/routes.tsx
@@ -2,8 +2,6 @@ import * as React from 'react'
 import { Route } from 'react-router-dom'
 import { Layout } from './components/Layout'
 import Home from './components/Home'
-import FetchData from './components/FetchData'
-import Counter from './components/Counter'
 import FlightSearch from './components/flightSearch'
 import FlightResult from './components/flightResult'
 import FlightPax from './components/FlightPax'
@@ -16,13 +14,8 @@ export const routes = <Layout>
   <Route exact path='/hotels/:country/:city/:hotelId' component={HotelDetail} />
   <Route exact path='/:locale/hotels/:country/:city' component={HotelResult} />
   <Route exact path='/hotels/:country/:city' component={HotelResult} />
-
   
-
-  
-  <Route path='/counter' component={Counter} />
   <Route path='/flight/search' component={FlightSearch} />
   <Route path='/flight/result/:route' component={FlightResult} />
   <Route path='/flight/pax/' component={FlightPax} />
-  <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
 </Layout>;
--- TravelConnect.React/ClientApp/store/index.ts
+++ TravelConnect.React/ClientApp/store/index.ts
@@ -1,21 +1,18 @@
-import * as WeatherForecasts from './WeatherForecasts';
-import * as Counter from './Counter';
-import * as Flight from './Flight';
+import * as Flight from './Flight'
+import * as Hotel from './Hotel'
 
 // The top-level state object
 export interface ApplicationState {
-  counter: Counter.CounterState
   flight: Flight.FlightState
-  weatherForecasts: WeatherForecasts.WeatherForecastsState
+  hotel: Hotel.HotelState
 }
 
 // Whenever an action is dispatched, Redux will update each top-level application state property using
 // the reducer with the matching name. It's important that the names match exactly, and that the reducer
 // acts on the corresponding ApplicationState property type.
 export const reducers = {
-  counter: Counter.reducer,
   flight: Flight.reducer,
-  weatherForecasts: WeatherForecasts.reducer
+  hotel: Hotel.reducer,
 };
 
 // This type can be used as a hint on action creators so that its 'dispatch' and 'getState' params are
