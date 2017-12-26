5d4195700e49cc862a4749faab0c7452cb3e23f5
niel <niel.heo@gmail.com>
2017-12-26 21:08:43 +0700

    Guest validation

--- TravelConnect.React/ClientApp/components/bookHotel/Guest.tsx
+++ TravelConnect.React/ClientApp/components/bookHotel/Guest.tsx
@@ -11,10 +11,52 @@ export default class BookHotel_Info extends React.Component<
   { recheckedRoomPrice: any, history: any }, any> {
   constructor(props: any) {
     super(props);
+
+    this.state = {
+      clicked: false,
+      title: '',
+      firstName: '',
+      lastName: '',
+      email: '',
+      confirmEmail: '',
+      errors: []
+    }
   }
 
   _onContinueClick = () => {
-    this.props.history.push('/thankyou')
+    let errors = []
+    if (!this.state.title) errors.push('Title is required')
+    if (!this.state.firstName) errors.push('First Name is required')
+    if (!this.state.lastName) errors.push('Last Name is required')
+    if (!this.state.email) errors.push('Email is required')
+    if (!this.state.confirmEmail) errors.push('Confirm Email is required')
+    if (this.state.confirmEmail !== this.state.email) errors.push('Confirm Email is not matched')
+
+    if (errors.length === 0)
+      this.props.history.push('/thankyou')
+
+    this.setState({ errors: errors })
+    this.setState({ clicked: true })
+  }
+
+  _onTitleChange = (e: any) => {
+    this.setState({ title: e.target.value })
+  }
+
+  _onFirstNameChange = (e: any) => {
+      this.setState({ firstName: e.target.value })
+  }
+
+  _onLastNameChange = (e: any) => {
+      this.setState({ lastName: e.target.value })
+  }
+
+  _onEmailChange = (e: any) => {
+      this.setState({ email: e.target.value })
+  }
+
+  _onConfirmEmailChange = (e: any) => {
+      this.setState({ confirmEmail: e.target.value })
   }
   
   public render() {
@@ -25,10 +67,10 @@ export default class BookHotel_Info extends React.Component<
         <Col md={1}>
           <FormDropdown
             label='Title'
-            value=''
+            value={this.state.title}
             disabled={false}
-            error=''
-            onChange={null}
+            error={ (this.state.clicked && !this.state.title) ? '* Required' : ''}
+            onChange={this._onTitleChange}
           >
             <option value=''></option>
             <option value='Mr.'>Mr.</option>
@@ -39,20 +81,20 @@ export default class BookHotel_Info extends React.Component<
         <Col md={3}>
           <FormTextbox
             label='First Name'
-            value=''
+            value={this.state.firstName}
             disabled={false}
-            error=''
-            onChange={null}
+            error={(this.state.clicked && !this.state.firstName) ? '* Required' : ''}
+            onChange={this._onFirstNameChange}
             
           />
         </Col>
         <Col md={3}>
           <FormTextbox
             label='Last Name'
-            value=''
+                    value={this.state.lastName}
             disabled={false}
-            error=''
-            onChange={null}
+            error={(this.state.clicked && !this.state.lastName) ? '* Required' : ''}
+            onChange={this._onLastNameChange}
           />
         </Col>
       </Row>
@@ -60,10 +102,10 @@ export default class BookHotel_Info extends React.Component<
         <Col md={5}>
           <FormTextbox
             label='Email Address'
-            value=''
+                    value={this.state.email}
             disabled={false}
-            error=''
-            onChange={null}
+            error={(this.state.clicked && !this.state.email) ? '* Required' : ''}
+            onChange={this._onEmailChange}
 
           />
         </Col>
@@ -72,10 +114,11 @@ export default class BookHotel_Info extends React.Component<
         <Col md={5}>
           <FormTextbox
             label='Confirm Email Address'
-            value=''
+                    value={this.state.confirmEmail}
             disabled={false}
-            error=''
-            onChange={null}
+            error={(this.state.clicked && !this.state.confirmEmail) ? '* Required'
+                : (this.state.confirmEmail !== this.state.email ? '* not matched' : '')}
+            onChange={this._onConfirmEmailChange}
           />
         </Col>
       </Row>
@@ -86,7 +129,14 @@ export default class BookHotel_Info extends React.Component<
           <RoomGuest recheckedRoomPrice={this.props.recheckedRoomPrice} index={idx++} />)
       }
       <Row>
-        <Col md={12}><Button bsStyle="primary" onClick={this._onContinueClick}>Continue</Button></Col>
+            <Col md={12}>
+                {
+                    this.state.errors.length ? <ul className='danger'>
+                        {this.state.errors.map((err: any) => <li>{err}</li>)}
+                    </ul> : null
+                }
+                <Button bsStyle="primary" onClick={this._onContinueClick}>Continue</Button>
+            </Col>
       </Row>
     </section>
   }
